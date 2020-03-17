42206f183821f86beaa86aba89caaa6af152b980
Edu Carreiro <53188149+edcarreiro@users.noreply.github.com>
2020-03-17 10:56:06 -0300

    included as a fucition in .zshrcfile

--- PlexZSHRCBackup.sh
+++
@@ -1,3 +0,0 @@
-zip -r -q ~/Temporary/PlexMediaServerBackup.zip ~/Library/Application\ Support/Plex\ Media\ Server
-mv ~/Temporary/PlexMediaServerBackup.zip ~/Dropbox/BACKUP
-cp .zshrc ~/Dropbox/BACKUP/zshrcBACKUP.txt
