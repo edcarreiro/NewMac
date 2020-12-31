86edce7c6d28142860acb351d0660efc11cf536a
Eduardo Carreiro <carreiro@wpcwebdesign.com>
2020-12-31 13:30:57 -0300

    Remade everything

--- Install-1.sh
+++ Install-1.sh
@@ -3,77 +3,142 @@ clear
 read -p "Are you already installed BITDEFENDER? If YES press [ENTER]"
 read -p "Are you Sure? If YES press [ENTER]"
 echo
-echo "Will be Installed:"
-echo "=================="
-echo
-read -p "Press [ENTER] to continue"
 clear
-echo ">>>>>>>>> Installing Homebrew!"
-echo
+
+
+
+echo "###########  Installing Homebrew and MAS! "
 sleep 3s
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install mas
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 echo
 echo
 echo
-echo ">>>>>>>>> Installing iTerm2! "
 echo
+echo
+echo
+
+
+
+echo "###########  Installing TERMINAL COMMAND APPS! "
 sleep 3s
-brew cask install iterm2
 echo
 echo
-echo
-echo ">>>>>>>>> Installing Atom! "
-echo
-sleep 3s
-brew cask install atom
-echo
-echo
-echo
-echo ">>>>>>>>> Installing exiftool and Amphetamine! "
-echo
-sleep 3s
+brew tap homebrew/cask-fonts
+brew tap homebrew/cask-versions
+brew install --cask font-fira-code
 brew install exiftool
 echo
 echo
-mas install 937984704   #Amphetamine
 echo
 echo
 echo
-echo ">>>>>>>>> Installing Fira Code Font! "
 echo
+
+
+
+echo"###########  Installing Brew Cask APPS! "
 sleep 3s
-brew tap homebrew/cask-fonts
 echo
 echo
-brew cask install font-fira-code
+mas install 937984704   #Amphetamine
+brew install --cask iterm2
+brew install --cask alfred
+brew install --cask google-chrome
+brew install --cask firefox-developer-edition
+brew install --cask 1password
+brew install --cask keka
+brew install --cask whatsapp
+brew install --cask signal
+brew install --cask microsoft-word
+brew install --cask microsoft-excel
+brew install --cask onedrive
+brew install --cask visual-studio-code
+brew install --cask plex-media-server
+brew install --cask camtasia
+brew install --cask mkvtoolnix
+brew install --cask aegisub
+brew install --cask vlc
+brew install --cask dropbox
+brew install --cask a-better-finder-rename
+brew install --cask imageoptim
+brew install --cask launchpad-manager
 echo
 echo
-echo  
-echo ">>>>>>>>> Installing OH-MY-ZSH "
-echo
-sleep 5s
-echo "After install, paste the clipboard in shell of OH-MY-ZSH to install POWERLEVEL10K. "
-echo "After installation, * QUIT * OH-MY-ZSH"
-echo "Press any key to continue (and wait 10s)!"
-pbcopy < ~/Projects/NewMac/p10kinstall.txt
-sleep 10s
-echo
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo
 echo
 echo
-echo ">>>>>>>>> Configuring .zshrc file!"
+echo
+
+
+echo"###########  Installing Mac Apple Store APPS! "
+sleep 3s
+echo
+mas install 510620098   #MediaInfo
+mas install 417375580   #BetterSnapTool
+mas install 946399090   #Telegram Desktop
+mas install 1489591003  #Edison Mail
+mas install 463362050   #PhotoSwepper
+mas install 1289583905  #Pixelmator Pro
+mas install 410968114   #PDF Scanner
+mas install 1437809329  #SurfShark VPN
+mas install 411643860   #DaisyDisk
+echo
+echo
+echo
+echo 
+echo
+echo
+
+
+
+echo"###########  Cleaning Up! "
+sleep 3s
+echo
+brew cleanup -v
+brew cleanup cask -v
+echo
+echo
+echo
+echo
+echo
+echo
+
+
+
+echo "###########  Downloading SETAPP "
+sleep 3s
+echo
+curl  -o "/Users/eduardo/Downloads/InstallSetapp.zip" "https://dl.devmate.com/com.setapp.InstallSetapp/InstallSetapp.zip" 
+echo
+echo
+echo
+echo
+echo 
+echo
+
+
+
+
+
+echo "###########  Configuring .zshrc file!"
 echo " " >> ~/.zshrc
 echo " " >> ~/.zshrc
+echo " " >> ~/.zshrc
+
 echo "####### CONFIG MADE BY INSTALLATION SCRIPT BY CARREIRO #######" >> ~/.zshrc
+
 echo "alias lsa=\"ls -la -Gh\"" >> ~/.zshrc
 echo "alias cd..=\"cd ..\"" >> ~/.zshrc
-echo "alias ˜=\"~\"" >> ~/.zshrc
-echo "alias frs10=\"flutter run -d RQ8M60ER93N\"" >> ~/.zshrc
-echo "alias fcreate=\"flutter create --androidx -t app\"" >> ~/.zshrc
-echo "alias gitpf=\"git push -f origin master\"" >> ~/.zshrc
-echo "alias python=\"python3\"" >> ~/.zshrc
+echo "alias ext=\"exiftool\"" >> ~/.zshrc
+
+
 echo " " >> ~/.zshrc
+echo " " >> ~/.zshrc
+echo " " >> ~/.zshrc
+
+
 echo "function delds {" >> ~/.zshrc
 echo "  rm -r -f ~/Movies/TV" >> ~/.zshrc
 echo "  find . -name '.DS_Store' -type f -delete" >> ~/.zshrc
@@ -81,44 +146,94 @@ echo "  find . -name '.localized' -type f -delete" >> ~/.zshrc
 echo "  rm -r -f ~/Music/Music/" >> ~/.zshrc
 echo "  brew cleanup -v" >> ~/.zshrc
 echo "  brew cleanup cask -v" >> ~/.zshrc
-echo "#  rm -R -f ~/Library/Caches/Homebrew/*" >> ~/.zshrc
-echo "#  rm *log.html" >> ~/.zshrc
-echo "}" >> ~/.zshrc
-echo " " >> ~/.zshrc
-echo "function find0 {" >> ~/.zshrc
 echo "  find . -size 0" >> ~/.zshrc
 echo "  find ./ -type f -size 0" >> ~/.zshrc
+echo "  find . -type f -size 0 -exec rm -f '{}' +" >> ~/.zshrc
+echo "}" >> ~/.zshrc
+
+
+echo " " >> ~/.zshrc
+echo " " >> ~/.zshrc
+echo " " >> ~/.zshrc
+
+
+echo "#### Function to correct date/time in video files ####" >> ~/.zshrc
+echo " " >> ~/.zshrc
+echo "function exm" >> ~/.zshrc
+echo "  exiftool $1 \"-time:all=$2-03:00\"" >> ~/.zshrc
 echo "}" >> ~/.zshrc
 echo " " >> ~/.zshrc
-echo "function backupzp {" >> ~/.zshrc
-echo "  zip -r -q ~/Temporary/PlexMediaServerBackup.zip ~/Library/Application\ Support/Plex\ Media\ Server" >> ~/.zshrc
-echo "  mv ~/Temporary/PlexMediaServerBackup.zip ~/Dropbox/BACKUP" >> ~/.zshrc
-echo "  cp .zshrc ~/Dropbox/BACKUP/zshrcBACKUP.txt" >> ~/.zshrc
-echo "}" >> ~/.zshrc
 echo " " >> ~/.zshrc
-echo " " >> ~/.zshrc
-echo "# Function to correct date/time in video files#" >> ~/.zshrc
-echo " " >> ~/.zshrc
-echo "function exifmp4" >> ~/.zshrc
-echo "  exiftool $1 \"-time:all=$2\"" >> ~/.zshrc
-echo "}" >> ~/.zshrc
-echo " " >> ~/.zshrc
-echo " " >> ~/.zshrc
+
+
 echo "### END OF CONFIG MADE BY INSTALLATION SCRIPT BY CARREIRO ###" >> ~/.zshrc
 echo " " >> ~/.zshrc
 echo " " >> ~/.zshrc
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+
+
+
+echo "TO DO: SETAPP software are downloaded in Downloads folder."
+echo "Just INSTALL!"
+echo "Affinity Suite must be download separated."
+echo "OH-MY-ZSH and PowerLevel10k Will be installed next. Wait!"
+sleep 6s
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+
+
+
+echo "########### Installing OH-MY-ZSH "
+sleep 5s
+echo
+echo "After install, paste the clipboard inside OH-MY-ZSH shell to install POWERLEVEL10K. "
+echo "After installation of PowerLevel10K, * QUIT * OH-MY-ZSH"
+pbcopy < ~/Projects/NewMac/p10kinstall.txt
+sleep 8s
+echo
+echo
+echo
+echo
+
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo
+echo
+echo
+
 echo "# ZSH_THEME=powerlevel10k/powerlevel10k   # Replace this line in the beginnging of this file" >> ~/.zshrc
 echo
 echo
-echo
-echo "TextEdit will be open to replace ZSH Theme"
-sleep 5s
-echo
-echo
-open -a TextEdit ~/.zshrc
-echo
-echo
-echo
-echo
-echo "FINISH. Don't forget to replace ZHS_THEME for POWERLEVEL10k in .zshrc file"
+
+
+echo "FINISH. Next don't forget to replace ZHS_THEME for POWERLEVEL10k in .zshrc file"
 echo "The line is alredy added in the file but must be replaced for the default configuration."
+echo "########### VSCode will be open to replace ZSH Theme in .zshrc FILE" 
+sleep 8s
+echo
+code ~/.zshrc
+echo
+echo
+echo
+echo
+echo
+git config --global user.name "Eduardo Carreiro"
+git config --global user.email "carreiro@wpcwebdesign.com"
+echo
+echo
+
+
+
+
+
--- readme.md
+++ readme.md
@@ -1,4 +1,3 @@
-
 # Scripts for Automatize Apps Installation in New Apple Mac's
 
 Script files to install automatically a lot of apps and resources when I re-format or buy a new Mac!
@@ -11,76 +10,56 @@ Almost all my apps are installed. Only some of then do not work.
 
 </br>
 
-### File install-1.sh ###
+### Terminal Command Apps :
 
-| Software | Way of Install | Type |
-|---|:-:|:-:|
-| brew | direct by internet | package manager |
-| iTerm2 | brew cask | terminal replacement |
-| Atom | brew cask | text editor |
-| brew mas | brew | extension to enable install Mac App Store by brew |
-| Amphetamina | mas | block Mac to sleep |
-| brew cask fonts | brew | extension to enable install fonts by brew |
-| Fira Code Font | brew | font for programming |
-| OH-MY-ZSH | shell direct by internet | framework for Mac terminal ZSH |
-| Powerlevel10k | git | theme for OH-MY-ZSH |
-| Configure .ZSHRC file | --- | configurations added to .zshrc |
+| Software       | Type                                       |
+| :------------- | ------------------------------------------ |
+| brew           | package manager                            |
+| Fira Code Font | font for programming                       |
+| exiftool       | correct date/time in audio and video files |
 
 </br>
 
-### File install-2.sh ###
+</br>
 
+### Brew Cask Apps :
 
-| Software | Way of Install | Type |
-|---|:-:|:-:|
-| Parallels | brew cask | virtual machine manager |
-| Microsoft Word | brew cask | office word processor |
-| Microsoft Excel | brew cask | office stylesheet |
-| Microsoft OneDrive | brew cask | cloud service |
-|   |  |  |
-| Android Studio | brew cask | IDE for flutter |
-| Visual Studio Code | brew cask | IDE |
-| Android File Transfer | brew cask | transfer files to mobile |
-|   |  |  |
-| Flutter | git | framework |
-| Dart | brew | compiler |
-|   |  |  |
-| MediaInfo | mas | media information |
-| BetterSnapTool | mas | window utility |
-| Better Rename 10 | mas | renamer  |
-| Telegram Desktop | mas | telegram Client |
-| Airmail 3 | mas | email client |
-| Pages | mas | Apple word processor |
-| Duplicate Photos Fixer Pro | mas | duplicate photo finder |
-| Pocket | mas | note utility |
-| Markoff | mas | markdown viewer |
-|   |  |  |
-| Google Chrome | brew cask | browser  |
-| Firefox | brew cask | browser |
-| Brave | brew cask | browser |
-| 1password | brew cask | password manager |
-| Keka | brew cask | compressor utility |
-| Whatsapp | brew cask | whatsapp client |
-| Signal | brew cask | signal client |
-| Cyberduck | brew cask | FTP Manager |
-| Windscribe | brew cask | VPN |
-| Plex | brew cask | media server |
-| Mkvtoolnix | brew cask | mkv file editor |
-| Avidemux | brew cask | mkv file cutter |
-| Aegisub | brew cask | subtitle editor |
-| Filebot | brew cask | video series renamer |
-| VLC | brew cask | media player |
-| 4k video downloader | brew cask | video downloader |
-| Launchpad manager | brew cask | launchpad utility |
-| Funter | brew cask | finder files |
-| Imageoptim | brew cask | image compressor |
-|   |  |  |
-| Xcode | mas | developer tool |
-|   |  |  |
-| Setapp | downloaded | app package signature |
-| Xtrafinder | downloaded | finder replacement |
-| Paragon NTFS | downloaded | NTFS reader/writter |
-| Python 3.8.2 | downloaded | compiler |
-| Affinity Photo | downloaded | photo editor |
-| Affinity Designer | downloaded | vector editor |
-| Affinity Publisher | downloaded | page designer |
+| Software                  |           Type            |
+| :------------------------ | :-----------------------: |
+| iTerm2                    |   terminal replacement    |
+| Alfred                    | Utility replace Spotlight |
+| Google Chrome             |     Internet browser      |
+| Firefox Developer Edition |          browser          |
+| 1password                 |     password manager      |
+| Keka                      |    compressor utility     |
+| Whatsapp                  |      whatsapp client      |
+| Signal                    |       signal client       |
+| Microsoft Word            |   office word processor   |
+| Microsoft Excel           |     office stylesheet     |
+| Microsoft OneDrive        |       cloud service       |
+| Visual Studio Code        |    programming editor     |
+| Plex                      |       media server        |
+| Camtasia                  |  video capture e editor   |
+| Mkvtoolnix                |      mkv file editor      |
+| Avidemux                  |      mkv file cutter      |
+| Aegisub                   |      subtitle editor      |
+| VLC                       |       media player        |
+| Dropbox                   |       cloud service       |
+| A Better Finder Rename 10 |       batch renamer       |
+| Imageoptim                |     image compressor      |
+| Launchpad manager         |     launchpad utility     |
+
+### Mac Apple Store Apps :
+
+| Software         |             type             |
+| :--------------- | :--------------------------: |
+| Amphetamine      |      sleep mac control       |
+| MediaInfo        | audio media exif information |
+| BetterSnapTool   |        window utility        |
+| Telegram Desktop |       telegram client        |
+| Edison           |         email client         |
+| PhotoSweeper     |    duplicate photo finder    |
+| Pixelamator Pro  |         photo editor         |
+| PDF Scanner      |         ocr for pdf          |
+| SurfShark        |             vpn              |
+| DaisyDisk        |             hdd              |
